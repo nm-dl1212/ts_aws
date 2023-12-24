c96d8a08f376c50c998970b45f0a52a219aeb2fb
nmatsu <n.matsumura2022@gmail.com>
2023-12-24 15:04:29 +0000

    mod: 2bucket

--- lib/aws_webapp-stack.ts
+++ lib/aws_webapp-stack.ts
@@ -1,15 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
 
 export class AwsWebappStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // s3
-    const bucket = new s3.Bucket(this, 'AwsS3Bucket', {
-      bucketName: 'test-bucket-fromcdk'
+    const bucket1 = new s3.Bucket(this, 'S3Bucket1', {
+      bucketName: 'test-bucket1-fromcdk',
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
+
+    // s3
+    const bucket2 = new s3.Bucket(this, 'S3Bucket2', {
+      bucketName: 'test-bucket2-fromcdk',
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
   }
 }

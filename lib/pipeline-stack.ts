59cfdf216e42c961c87bd3d90dd46b5a1700dbcd
nmatsu <n.matsumura2022@gmail.com>
2023-12-25 16:27:18 +0000

    mod: master->mai

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -20,7 +20,7 @@ export class PipelineStack extends cdk.Stack {
         const pipeline = new CodePipeline(this, 'Pipeline', {
             pipelineName: 'WorkshopPipeline',
             synth: new CodeBuildStep('SynthStep', {
-                input: CodePipelineSource.codeCommit(repo, 'master'),
+                input: CodePipelineSource.codeCommit(repo, 'main'),
                 installCommands: [
                     'npm install -g aws-cdk'
                 ],
